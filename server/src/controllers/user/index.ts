56649d7da22ec9ccededa6667292fcae2035961d
Khánh Huân <www.khanhhuan@gmail.com>
2025-02-03 14:59:27 +0700

    create: create new user

--- server/src/controllers/user/index.ts
+++ server/src/controllers/user/index.ts
@@ -11,6 +11,27 @@ class UserController {
     }
     return res.status(200).json(users);
   });
+
+  createUser = asyncWrapper(async (req: Request, res: Response) => {
+    const {
+      username,
+      cognitoId,
+      profilePictureUrl = "i1.jpg",
+      teamId = 1,
+    } = req.body;
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        cognitoId,
+        profilePictureUrl,
+        teamId,
+      },
+    });
+    if (!newUser) {
+      return res.status(400).json({ message: "User not created" });
+    }
+    return res.status(201).json(newUser);
+  });
 }
 
 const userController = new UserController();
--- server/src/routes/user/index.ts
+++ server/src/routes/user/index.ts
@@ -3,5 +3,6 @@ import { userController } from "../../controllers";
 const userRouter = Router();
 
 userRouter.get("/", userController.getUsers);
+userRouter.post("/create-user", userController.createUser);
 
 export default userRouter;
