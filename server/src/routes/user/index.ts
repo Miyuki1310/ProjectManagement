95fac150a46956d25e3087dfa8a6f29a9f9f24f8
Khánh Huân <www.khanhhuan@gmail.com>
2025-02-06 11:20:13 +0700

    Dix: register user

--- server/src/routes/user/index.ts
+++ server/src/routes/user/index.ts
@@ -3,7 +3,7 @@ import { userController } from "../../controllers";
 const userRouter = Router();
 
 userRouter.get("/", userController.getUsers);
-userRouter.post("/create-user", userController.createUser);
+userRouter.post("/", userController.createUser);
 userRouter.get("/:cognitoId", userController.getUser);
 
 export default userRouter;
